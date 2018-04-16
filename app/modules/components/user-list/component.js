c7a2fa37ed1b62f0b739d417734952b03909932b
WangSen <wsgoodman@163.com>
2018-04-16 17:59:13 +0800

    添加样式及新组件

--- app/modules/components/user-list/component.js
+++ app/modules/components/user-list/component.js
@@ -1,20 +1,64 @@
 import Component from '@ember/component';
 import styles from '../../github-users/styles';
-import set from '@ember/object';
-
+import { set, get } from '@ember/object';
+import {  inject } from '@ember/service';
 export default Component.extend({
-    styles,
-    isShowUsers: true,
-    selectUser: null,
-    actions: {
-        showUsers() {
-            set(this, 'isShowUsers', true);
-            set(this, 'selectUser', null);
-        },
-        getUserInfo(user) {
-            console.info(user)
-            set(this, 'isShowUsers', false);
-            set(this, 'selectUser', user);
-        }
+  styles,
+  isShowUsers: true,
+  selectUser: null,
+  userFollowings: [{
+      id: 1,
+      login: "“aaa”"
+    },
+    {
+      id: 2,
+      login: "“bbb”"
+    },
+    {
+      id: 3,
+      login: "ccc"
     }
+  ],
+  ajax: inject(),
+
+  actions: {
+    showUsers() {
+      set(this, 'isShowUsers', true);
+      set(this, 'selectUser', null);
+    },
+    getUserInfo(user) {
+      console.info(user)
+      set(this, 'isShowUsers', false);
+      set(this, 'selectUser', user);
+    },
+    getUserFollowings(user) {
+      let userFollowingsData = this.get('ajax')
+        .request('/users/' + user.login + '/following')
+        .then((followings) => {
+          set(this, 'userFollowings', followings)
+        })
+    },
+    onClick() {
+        console.log("click2")
+    },
+
+  },
+  // onClick() {
+  //     console.log(this)
+  //     // get(this,'ajax')
+  //     //   .request('/users/' + user.login + '/following')
+  //     //   .then((followings) => {
+  //     //     set(this, 'userFollowings', followings)
+  //     //   })
+  // },
+
+    // click(e) {
+    //   console.log(this)
+    //   // e.preventDefault();
+    //   // let a = "fuck0";
+    //   // this.get('ajax')
+    //   // this.get('onClick')(a);
+    //   // console.log('this was clicked');
+    //
+    // }
 });
