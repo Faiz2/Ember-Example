71b9ba6612a2e83b52ae359592391b36d91e4359
Alex <891366402@qq.com>
2018-04-19 00:52:58 +0800

    index路由问题

--- app/modules/application/route.js
+++ app/modules/application/route.js
@@ -2,22 +2,5 @@ import Route from '@ember/routing/route';
 import { inject } from '@ember/service';
 
 export default Route.extend({
-    ajax: inject(),
-    beforeModel(transition) {
-        const ajax = this.get('ajax');
-        ajax.host = 'https://api.github.com';
-        const { queryParams } = transition;
-        this.remindAuth = null;
-        let token  = window.localStorage.getItem('github_api_oauth');
-        if (token) {
-            ajax.headers['Authorization'] = `token ${token}`;
-        } else {
-            this.remindAuth = "还没有OAurh认证";
-        }
-    },
-    setupController(controller, model) {
-        this._super(controller, model);
-        controller.set('remindAuth', this.remindAuth);
-    }
-
+    
 });
