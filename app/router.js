e22d5158a5bb52ba814380b1f634f1e7a7a27d60
Faiz <891366402@qq.com>
2018-07-20 19:38:59 +0800

    temp

--- app/router.js
+++ app/router.js
@@ -2,8 +2,8 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL
 });
 
 Ember.Route.reopen({
@@ -15,13 +15,22 @@ Ember.Route.reopen({
 });
 
 Router.map(function() {
-  this.route('oauth-callback');
-  this.route('github-users', function() {});
-  this.route('example-file-upload');
-  this.route('example-forms');
-  this.route('page-not-found', {path: '/*path'});
-  this.route('max-demo');
-  this.route('echarts-demo');
+    this.route('oauth-callback');
+    this.route('github-users', function() {});
+    this.route('example-file-upload');
+    this.route('example-forms');
+    this.route('page-not-found', { path: '/*path' });
+    this.route('max-demo', function() {
+      this.route('t1');
+      this.route('t2');
+      this.route('t3');
+
+      this.route('index', function() {
+        this.route('a1');
+      });
+    });
+    this.route('echarts-demo');
+
 });
 
 export default Router;
---
+++ tests/unit/modules/max-demo/index/a1/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:max-demo/index/a1', 'Unit | Route | max demo/index/a1', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
